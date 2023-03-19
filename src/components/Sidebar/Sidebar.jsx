6cf351ac74e6381a6bfcbf18c1fcb1f3f72a1004
vanthang24803 <nguyenthang24803@gmail.com>
2023-03-19 21:36:11 +0700

    responsive tablet

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -11,72 +11,78 @@ import Contact from '../Contact/Contact';
 
 const Sidebar = () => {
     return (
-        <aside className='aside'>
-            <a href="#home" className="nav__logo">
-                <img src={Logo} alt="" />
-            </a>
+        <>
+            <aside className='aside'>
+                <a href="#home" className="nav__logo">
+                    <img src={Logo} alt="" />
+                </a>
 
-            <nav className='nav'>
-                <div className="nav__menu">
-                    <ul className="nav__list">
-                        <li className='nav_item'>
-                            <a href="#home" className="nav__link">
-                                <i className="icon-home"></i>
-                            </a>
-                        </li>
+                <nav className='nav'>
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            <li className='nav_item'>
+                                <a href="#home" className="nav__link">
+                                    <i className="icon-home"></i>
+                                </a>
+                            </li>
 
-                        <li className='nav_item'>
-                            <a href="#about" className="nav__link">
-                                <i className="icon-user-following"></i>
-                            </a>
-                        </li>
+                            <li className='nav_item'>
+                                <a href="#about" className="nav__link">
+                                    <i className="icon-user-following"></i>
+                                </a>
+                            </li>
 
-                        <li className='nav_item'>
-                            <a href="#services" className="nav__link">
-                                <i className="icon-briefcase"></i>
-                            </a>
-                        </li>
+                            <li className='nav_item'>
+                                <a href="#services" className="nav__link">
+                                    <i className="icon-briefcase"></i>
+                                </a>
+                            </li>
 
-                        <li className='nav_item'>
-                            <a href="#resume" className="nav__link">
-                                <i className="icon-graduation"></i>
-                            </a>
-                        </li>
+                            <li className='nav_item'>
+                                <a href="#resume" className="nav__link">
+                                    <i className="icon-graduation"></i>
+                                </a>
+                            </li>
 
-                        <li className='nav_item'>
-                            <a href="#work" className="nav__link">
-                                <i className="icon-layers"></i>
-                            </a>
-                        </li>
+                            <li className='nav_item'>
+                                <a href="#work" className="nav__link">
+                                    <i className="icon-layers"></i>
+                                </a>
+                            </li>
 
-                        <li className='nav_item'>
-                            <a href="#pricing" className="nav__link">
-                                <i className="icon-bulb"></i>
-                            </a>
-                        </li>
+                            <li className='nav_item'>
+                                <a href="#pricing" className="nav__link">
+                                    <i className="icon-bulb"></i>
+                                </a>
+                            </li>
 
-                        <li className='nav_item'>
-                            <a href="#blog" className="nav__link">
-                                <i className="icon-note"></i>
-                            </a>
-                        </li>
+                            <li className='nav_item'>
+                                <a href="#blog" className="nav__link">
+                                    <i className="icon-note"></i>
+                                </a>
+                            </li>
 
-                        <li className='nav_item'>
-                            <a href="#contact" className="nav__link">
-                                <i className="icon-bubble"></i>
-                            </a>
-                        </li>
-                    </ul>
+                            <li className='nav_item'>
+                                <a href="#contact" className="nav__link">
+                                    <i className="icon-bubble"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+
+                <div className="nav__footer">
+                    <span className="copyright">
+                        &copy; 2023 - 2024.
+                    </span>
                 </div>
-            </nav>
+            </aside>
 
-
-            <div className="nav__footer">
-                <span className="copyright">
-                &copy; 2023 - 2024.
-                </span>
+            <div className="nav__toggle">
+                <i className="icon-menu"></i>
             </div>
-        </aside>
+        </>
     )
 }
 
